5afcc4466a92b85c44781243f607e3f7bb53eb88
Dave Trost <davetrost@gmail.com>
2019-10-29 23:53:56 -0700

    more characters

--- src/components/character-list.js
+++ src/components/character-list.js
@@ -12,6 +12,30 @@ const characters = [
     name: 'Two Guys with Handlebar Mustaches',
     species: 'Human',
   },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/155.jpeg',
+    name: 'Harold',
+    species: 'Cronenberg',
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/426.jpeg',
+    name: 'Greasy Grandma',
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/487.jpeg',
+    name: 'Visor Rick',
+    species: 'Human',
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/265.jpeg',
+    name: 'Pickle Rick',
+    species: 'unknown, Pickle',
+  },
+  {
+    img: 'https://rickandmortyapi.com/api/character/avatar/424.jpeg',
+    name: 'Pizza-person',
+    species: 'Humanoid, Pizza',
+  },
 ];
 
 const CharacterList = () => {
