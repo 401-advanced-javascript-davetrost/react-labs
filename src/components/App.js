e40749ee70e62e465774da4b8a53b04c08c202d0
Dave Trost <davetrost@gmail.com>
2019-10-29 17:05:49 -0700

    header and footer

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header.js';
+import Footer from './footer.js';
+
+const App = () => (
+  <React.Fragment>
+    <Header />
+    <Footer />
+  </React.Fragment>
+);
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>&copy; Dave T (aka: the most ignorant Rick and Morty human being you have ever met)</footer>
+);
+
+export default Footer;
