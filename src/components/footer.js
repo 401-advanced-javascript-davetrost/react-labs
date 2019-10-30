26a1d52890f9e5c921403d2f6cf13f449ae78f5c
Dave Trost <davetrost@gmail.com>
2019-10-29 23:45:02 -0700

    footer css

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './footer.css';
 
 const Footer = () => (
-  <footer>&copy; Dave T (aka: the most ignorant Rick and Morty human being you have ever met)</footer>
+  <footer className={styles.Footer}>&copy; Dave T (aka: the most ignorant Rick and Morty human being you have ever met)</footer>
 );
 
 export default Footer;
